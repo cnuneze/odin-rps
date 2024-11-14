eb8be3c11a3fe5ae89de7d326a41125cc136f6b3
cnuneze <23690843+cnuneze@users.noreply.github.com>
2024-11-14 13:50:06 -0300

    Fix: Add computer choice function

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log('holita');
+function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random() * 100) % 3;
+
+    switch (randomNumber) {
+        case 0:
+            return 'Rock'
+        
+        case 1:
+            return 'Papper'
+        
+        case 2:
+            return 'Scissors'
+            
+        default:
+            break;
+    }
+}

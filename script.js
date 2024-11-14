f0e6580a6efa8972588896a51637ebb340b55a9f
cnuneze <23690843+cnuneze@users.noreply.github.com>
2024-11-14 16:18:45 -0300

    Fix: Add computer choice function

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 100) % 3;
 
-    switch (randomNumber) {
+    return getOption(randomNumber);
+}
+
+function getHumanChoice() {
+    console.log(`Select your choice:
+        1. Rock
+        2. Papper
+        3. Scissors
+    `);
+    
+    let humanChoice = parseInt(prompt("Input the number of your choice:"));
+
+    if (isNaN(humanChoice) || humanChoice > 4 || humanChoice < 0) {
+        console.log("The selected option is invalid.")
+        return;
+    }
+
+    return getOption(humanChoice - 1);
+}
+
+function getOption(option) {
+    switch (option) {
         case 0:
             return 'Rock'
         
